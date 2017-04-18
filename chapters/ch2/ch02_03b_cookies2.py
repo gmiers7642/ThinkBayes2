d4c255367611ee9275baf916ecfdc95dbb05a323
glenn <gmiers7642@gmail.com>
2017-04-18 18:25:27 -0500

    Added object implementation of cookie problem

---
+++ chapters/ch2/ch02_03b_cookies2.py
@@ -0,0 +1,34 @@
+from thinkbayes2 import Pmf
+
+class Cookie(Pmf):
+
+    def __init__(self, hypos):
+        Pmf.__init__(self)
+        for hypo in hypos:
+            self.Set(hypo, 1)
+        self.Normalize()
+        self.mixes = {
+                      'Bowl 1':dict(vanilla=0.75,    chocolate=0.25),
+                      'Bowl 2':dict(vanilla=0.5,    chocolate=0.5)
+                      }
+
+    def Update(self, data):
+        for hypo in self.Values():
+            like = self.Likelihood(data, hypo)
+            self.Mult(hypo, like)
+        self.Normalize()
+
+    def Likelihood(self, data, hypo):
+        mix = self.mixes[hypo]
+        like = mix[data]
+        return like
+
+if __name__ == '__main__':
+    hypos = ['Bowl 1', 'Bowl 2']
+    pmf = Cookie(hypos)
+
+    pmf.Print()
+
+    pmf.Update('vanilla')
+
+    pmf.Print()
